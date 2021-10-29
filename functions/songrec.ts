eb266f854196c21d91c6db0887cf4ffea259d9c0
khreate <surajkhosla79@gmail.com>
2021-10-29 11:41:48 -0500

    spotify web api still annoying, need to get bearer token

--- functions/songrec.ts
+++ functions/songrec.ts
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from "axios"; // import axios
+var SpotifyWebApi = require('spotify-web-api-node'); // import spotify-web-api-node
 
-export function songrec(song: string, artist: string, genre: string) {
-    console.log(axios.get(`https://api.spotify.com/v1/recommendations?limit=1&market=ES&seed_artists=${artist}&seed_genres=${genre}&seed_tracks=${song}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQAaIp0-h_b2cPem2yZ-OP-hvduJagOxn6rg0MZCHVhLORSUraGhWale8kvDs4shbeLBVyhwDQF4210bUzrK0y_kKUdQoJTs2ubZh_355JY7uVzbhnIP7ZH7DE-4O821zI0hic7NK96IXfxT3s3Yyxs16UF46_RstaJi8_lWTBM_FBDyucAmTHi0iwBdfEZWoeId1nx30ZLsu8-s`)
-    .then(response => {
-        console.log(response.data);
-        return response.data;
-    })
-    .catch(error => {
-        console.log(error);
-    })
-)}
+var spotifyApi = new SpotifyWebApi({ // create new spotify-web-api-node object
+    clientId: process.env.cid,
+    clientSecret: process.env.cs,
+    redirectUri: 'http://localhost:3000/callback'
+});
+
+
